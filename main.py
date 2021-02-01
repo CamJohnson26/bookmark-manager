2eca106d627d2b7588a04e7e01052603f4f09027
Cameron Johnson <cameronrossjohnson@gmail.com>
2021-01-31 18:50:56 -0600

    Got summarization working!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import re
 
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from transformers import pipeline
+
 import os
 import psycopg2
 import requests
@@ -10,11 +12,13 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 database = psycopg2.connect(DATABASE_URL)
 
+summarization = pipeline("summarization")
 
 def get_all_urls():
     cursor = database.cursor()
     cursor.execute("SELECT * FROM url")
     records = cursor.fetchall()
+    print(f"Fetched {len(records)} records")
     return [db_record_to_url_record(r) for r in records]
 
 
@@ -25,7 +29,8 @@ def db_record_to_url_record(db_record):
         "url": db_record[2],
         "title": db_record[3],
         "text": db_record[4],
-        "html": db_record[5]
+        "html": db_record[5],
+        "summary": db_record[6]
     }
 
 
@@ -37,6 +42,7 @@ def url_record_to_db_record(db_record):
         db_record["title"],
         db_record["text"],
         db_record["html"],
+        db_record["summary"],
     ]
 
 
@@ -61,12 +67,16 @@ def fetch_url(url):
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
     }
-    html = requests.get(url, headers=headers)
+    try:
+        html = requests.get(url, headers=headers)
 
-    # Fixes this bug: https://stackoverflow.com/questions/57371164/django-postgres-a-string-literal-cannot-contain-nul-0x00-characters
-    text = html.text.replace('\x00', '')
+        # Fixes this bug: https://stackoverflow.com/questions/57371164/django-postgres-a-string-literal-cannot-contain-nul-0x00-characters
+        text = html.text.replace('\x00', '')
 
-    return text
+        return text
+    except Exception:
+        print(f"Couldn't fetch {url}")
+        return ''
 
 
 def read_urls_from_file():
@@ -86,7 +96,7 @@ def wipe_ingest_file():
 def update_record(url):
     db_record = url_record_to_db_record(url)
     cursor = database.cursor()
-    cursor.execute("UPDATE url SET title = %s, text = %s, html = %s WHERE id = %s", [db_record[3], db_record[4], db_record[5], db_record[0]])
+    cursor.execute("UPDATE url SET title = %s, text = %s, html = %s, summary = %s WHERE id = %s", [db_record[3], db_record[4], db_record[5], db_record[6], db_record[0]])
     database.commit()
 
 
@@ -95,6 +105,14 @@ def create_record(url):
     cursor.execute("INSERT INTO url (id, created_at, url, title, text, html) VALUES (DEFAULT, DEFAULT, %s, %s, %s, %s)", [url, None, None, None])
     database.commit()
     print(f"Created: {url}")
+
+
+def get_summary(text):
+    # https://www.thepythoncode.com/article/text-summarization-using-huggingface-transformers-python
+    if len(text) >= 1024:
+        text = text[:1024]
+    summary = summarization(text)[0]['summary_text']
+    return summary
 
 
 def create_new_urls(urls):
@@ -116,13 +134,27 @@ def fill_in_missing_fields(urls):
             url["text"] = get_text_from_html(html)
 
 
+def fill_in_summary_field(urls):
+    for url in urls:
+        text = url["text"]
+        if url["summary"] is None and (text is not None):
+            summary = get_summary(text)
+            url["summary"] = summary
+            print(f"Summarized {url['url']}: {summary}")
+
+
 ingest_urls = read_urls_from_file()
 create_new_urls(ingest_urls)
 wipe_ingest_file()
 
 urls = get_all_urls()
-print(fill_in_missing_fields(urls))
+fill_in_missing_fields(urls)
 
+for url in urls:
+    update_record(url)
+    print(f"Updated: {url}")
+
+fill_in_summary_field(urls)
 for url in urls:
     update_record(url)
     print(f"Updated: {url}")
