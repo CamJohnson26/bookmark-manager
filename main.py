535ba001c99f3d9af1a69ad9ce77b3b4927fd5bf
Cameron Johnson <cameronrossjohnson@gmail.com>
2024-07-14 21:18:15 -0500

    Sleeping a new way

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from dotenv import load_dotenv
 
 from bookmark_library.queue.background_job import initiate_background_tasks
-import threading
+import asyncio
 
 load_dotenv()
+
+
+async def main():
+    # Initialize your background job or server here
+
+    # Keep the event loop running
+    while True:
+        await asyncio.sleep(3600)  # Sleep for an hour
+
 
 if __name__ == '__main__':
     print("Consuming messages from the queue")
     initiate_background_tasks()
+    asyncio.run(main())
 
-    # Keep the main thread running
-    stop_event = threading.Event()
-    stop_event.wait()
