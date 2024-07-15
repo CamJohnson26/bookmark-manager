f4749e28167857cabdd0a2a7cde0e07c2c77ebed
Cameron Johnson <cameronrossjohnson@gmail.com>
2024-07-14 21:06:45 -0500

    Keep script running

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 
 from bookmark_library.queue.background_job import initiate_background_tasks
+import threading
 
 load_dotenv()
 
 if __name__ == '__main__':
     print("Consuming messages from the queue")
     initiate_background_tasks()
+
+    # Keep the main thread running
+    stop_event = threading.Event()
+    stop_event.wait()
