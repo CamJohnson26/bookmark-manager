74a7ccd6ecb80cfa87432f4b597cb1d0804e13ac
Cameron Johnson <cameronrossjohnson@gmail.com>
2024-07-14 22:12:30 -0500

    Another

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install -y libxml2-dev libxslt-dev python3-dev build-essential
+sudo apt-get install -y libxml2-dev libxslt-dev python3-dev build-essential python3-distutils
